66c480098a1e1e3b14c0bf1221e68e7261523f4b
Dan Kinder <d.kinder02@gmail.com>
2020-12-17 12:29:37 +0000

    Layers and Popups

--- src/Configuration.js
+++ src/Configuration.js
@@ -1,6 +1,7 @@
 import Leaflet from 'leaflet'
-import { gulliesPopup} from './Popups' //devsitesPopup, notdevsitesPopup}
-import { gulliesStyle} from './Styles'
+import { greenbeltStyle, aqmaStyle, smokecontrolareaStyle } from './Styles'
+import { greenbeltPopup, aqmaPopup, smokecontrolareaPopup} from './Popups' 
+
 
 const Configuration = {
     Map: {
@@ -15,15 +16,38 @@ const Configuration = {
     },
     DynamicData: 
     [
+        
         {
-            key: 'Gullies',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=highway_assets:gully_faults_live&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            key: 'Smoke Control Areas',
+            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=climatology:smoke_control_area&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
             layerOptions: {
-                maxZoom: 16,
-                onEachFeature: gulliesPopup,
-                pointToLayer: (feature, latlng) => {
-                    return Leaflet.circleMarker (latlng, gulliesStyle (feature))
-                }
+                maxZoom: 2,
+                style: smokecontrolareaStyle,
+                onEachFeature: smokecontrolareaPopup
+            },
+            displayOverlay: true,
+            visibleByDefault: true
+        },
+        
+        {
+            key: 'Green Belt',
+            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=planning:green_belt_os&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            layerOptions: {
+                maxZoom: 2,
+                style: greenbeltStyle,
+                onEachFeature: greenbeltPopup
+            },
+            displayOverlay: true,
+            visibleByDefault: true
+        },
+
+        {
+            key: 'Air Quality Management Areas',
+            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=climatology:air_quality_management_areas&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            layerOptions: {
+                maxZoom: 2,
+                style: aqmaStyle,
+                onEachFeature: aqmaPopup
             },
             displayOverlay: true,
             visibleByDefault: true
