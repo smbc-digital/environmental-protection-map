1fb8ff8033f88a275ca9a462d602431e3d23de11
stephen.lally <stephen.lally.smbc@gmail.com>
2021-03-02 12:22:48 +0000

    fix(popup): fixed branded icon

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -2,7 +2,7 @@ const greenbeltPopup = (feature, layer) => {
   
   const content = `<div class="smbc-map__item">
   <div class="smbc-map__item__header__block">
-    <i class="fa fa-envira smbc-map__item__header__block__icon" aria-hidden="true"></i>
+    <i class="fab fa-envira smbc-map__item__header__block__icon" aria-hidden="true"></i>
     <span class="smbc-map__item__header__block__title">Green Belt</span>
   </div>
 </div>`
